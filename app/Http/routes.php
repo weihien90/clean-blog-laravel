0b06eac881a4fa4bbe86078ccbb0f9008ebea693
weihien <weihien@weihien.xyz>
2016-08-20 12:10:50 -0400

    Add routes for auth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,4 +11,13 @@
 |
 */
 
+// Homepage
 Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
+
+// Authentication routes
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+
