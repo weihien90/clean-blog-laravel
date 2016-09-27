0aeeebdc8fad27a55cc513f08f6e26229c6e2c5c
weihien90 <weihien90@gmail.com>
2016-09-27 09:36:08 +0800

    Update the header of Edit Post page

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -11,14 +11,14 @@
 @section('content')
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('{{ asset("img/home-bg.jpg") }}'">
+    <header class="intro-header" style="background-image: url('/img/edit-post.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Clean Blog</h1>
+                        <h1>Edit Post</h1>
                         <hr class="small">
-                        <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                        <span class="subheading">{{ $post->title }}</span>
                     </div>
                 </div>
             </div>
